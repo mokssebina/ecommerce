6a98fdaf290be362e5f123131185946fcae51dbb
Mookamedi Sebina <m.sebina@sais-erp.com>
2022-12-12 23:25:26 +0200

    Real estate portal development

--- src/components/ImageScrollBar.js
+++ src/components/ImageScrollBar.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { Box, Icon, Flex } from "@chakra-ui/react";
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
-
+/*----------*/
 
 const LeftArrow = () => {
   const { scrollPrev } = useContext(VisibilityContext);
