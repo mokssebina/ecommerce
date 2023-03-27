9b13a276bfa467776a73b124d4d78f4c3381b813
mokssebina <sebina.moks@gmail.com>
2023-03-27 07:44:50 +0200

    preview and listings creation display picture

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -30,6 +30,7 @@ function Profile({ children }) {
         
         <header className='fixed w-full shadow-md z-20 bg-white'>
           <div className='w-full lg:w-9/12 max-w-screen-2xl mx-auto bg-white'>
+           <p className='text-md mt-2 mb-2 text-center'>Orders</p>
           <div className='w-full h-10 flex mt-2'>
             <div className='w-9/12 px-1 py-1'>
             <input placeholder='Search' className='w-11/12 ml-1 h-full bg-gray-200 rounded-md md:w-2/4 lg:w-6/12 px-1 border-0' />
@@ -40,7 +41,6 @@ function Profile({ children }) {
           </div> 
           </div>  
           <div className='w-full lg:w-9/12 max-w-screen-2xl mx-auto h-14 bg-white relative z-10 mb-2'>
-           <p className='text-md mt-2 mb-2 text-center'>Orders</p>
           <div className='w-9/12 h-full flex justify-between items-center text-sm font-semibold text-gray-800'>
             
             <div className='items-center ml-2'>
